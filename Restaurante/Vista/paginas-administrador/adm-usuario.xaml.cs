9e0261da70aa5d5277aba41dcabb5b8bace4cf07
Moroxo <55703533+Moroxo@users.noreply.github.com>
2021-12-07 17:33:19 -0300

    Add files via upload

--- Restaurante/Vista/paginas-administrador/adm-usuario.xaml.cs
+++ Restaurante/Vista/paginas-administrador/adm-usuario.xaml.cs
@@ -16,6 +16,8 @@ using System.Data.EntityClient;
 using System.Data.Objects;
 using System.Data;
 using System.Data.OracleClient;
+//using RESTAURANTE.CONTROLADOR;
+
 
 
 namespace Restaurante.Vista.paginas_administrador
@@ -25,8 +27,12 @@ namespace Restaurante.Vista.paginas_administrador
     /// </summary>
     public partial class adm_usuario : Page
     {
-        String Conexion = "Data Source=localhost:1521/xe; password=123456; User id=RESTAURANT";
-        OracleConnection cone = new OracleConnection();
+        string correo; string estado; string rut; string nombre; string apellido;//SP_CREAR_EMPLEADOS
+        
+        static String Conexion = "Data Source=localhost:1521/orcl; password=rest; User id=restaurante";
+        static OracleConnection cone = new OracleConnection();
+
+
         public adm_usuario()
         {
             InitializeComponent();
@@ -39,10 +45,12 @@ namespace Restaurante.Vista.paginas_administrador
             lblpass.Visibility = Visibility.Visible;
             lbltipo.Visibility = Visibility.Visible;
             lblnomusuario.Visibility = Visibility.Visible;
+            lb_apellido.Visibility = Visibility.Visible;
             txtCorreo.Visibility = Visibility.Visible;
             txtEstado.Visibility = Visibility.Visible;
-            txtnomusuario.Visibility = Visibility.Visible;
-            pwboxpassword.Visibility = Visibility.Visible;
+            txtnom.Visibility = Visibility.Visible;
+            txt_rut.Visibility = Visibility.Visible;
+            txt_apellido.Visibility = Visibility.Visible;
             cbtipo.Visibility = Visibility.Visible;
             Crear_confirmacion.Visibility = Visibility.Visible;
         }
@@ -51,12 +59,21 @@ namespace Restaurante.Vista.paginas_administrador
         {
             cone.ConnectionString = Conexion;
             cone.Open();
+
+            OracleCommand cmd = new OracleCommand("SP_LISTAR_USUARIOS", cone);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("registros", OracleType.Cursor).Direction = ParameterDirection.Output;
+
+            OracleDataAdapter adaptador = new OracleDataAdapter();
+            adaptador.SelectCommand = cmd;
+
             DataTable dt = new DataTable();
-            String lector = "select * from usuario";
-            OracleDataAdapter adaptador = new OracleDataAdapter(lector, Conexion);
             adaptador.Fill(dt);
             dgUsuarios.ItemsSource = dt.AsDataView();
             dgUsuarios.Items.Refresh();
+
+
+
             cone.Close();
         }
         //Ocultamos los controles para crear un nuevo usuario en caso de no ser necesario su uso(al querer eliminar, listar o actualizar)
@@ -68,10 +85,12 @@ namespace Restaurante.Vista.paginas_administrador
             lblpass.Visibility = Visibility.Hidden;
             lbltipo.Visibility = Visibility.Hidden;
             lblnomusuario.Visibility = Visibility.Hidden;
+            lb_apellido.Visibility = Visibility.Hidden;
             txtCorreo.Visibility = Visibility.Hidden;
             txtEstado.Visibility = Visibility.Hidden;
-            txtnomusuario.Visibility = Visibility.Hidden;
-            pwboxpassword.Visibility = Visibility.Hidden;
+            txtnom.Visibility = Visibility.Hidden;
+            txt_rut.Visibility = Visibility.Hidden;
+            txt_apellido.Visibility = Visibility.Hidden;
             cbtipo.Visibility = Visibility.Hidden;
             Crear_confirmacion.Visibility = Visibility.Hidden;
         }
@@ -105,6 +124,7 @@ namespace Restaurante.Vista.paginas_administrador
                 tipo = 6;
             return tipo;
         }
+
         //Cuando el administrador desee crear un nuevo usuario apareceran los controles necesarios
         private void btnCrear_Click(object sender, RoutedEventArgs e)
         {
@@ -125,24 +145,30 @@ namespace Restaurante.Vista.paginas_administrador
             ocultarCrear();
             eliminartxt.IsEnabled = true;
             confirmar.IsEnabled = true;
+
+            listar();
         }
         //El administrador confirma que desea eliminar y se realiza el proceso junto a la base de datos
         private void confirmar_Click(object sender, RoutedEventArgs e)
         {
-            if(eliminartxt.Text!="")
+            
+            if (eliminartxt.Text!="")
             {
                 //Se abre la conexion
+            
                 cone.ConnectionString = Conexion;
                 cone.Open();
                 //Se envia el nombre del usuario a eliminar
-                String lector = "delete from usuario where nom_usuario ='" + eliminartxt.Text + "'";
-                OracleDataAdapter adaptador = new OracleDataAdapter(lector, Conexion);
-                DataTable dt = new DataTable();
-                adaptador.Fill(dt);
-                dgUsuarios.ItemsSource = dt.AsDataView();
-                dgUsuarios.Items.Refresh();
+
+                OracleCommand cmd = new OracleCommand("SP_DESCACTIVAR_USUARIO", cone);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("nombre", OracleType.Char).Value = eliminartxt.Text;
                 cone.Close();
-            }else
+                listar();
+                MessageBox.Show("usuario desactivado");
+
+            }
+            else
             {
                 MessageBox.Show("Debe ingresar nombre de usuario a eliminar");
             }
@@ -151,23 +177,41 @@ namespace Restaurante.Vista.paginas_administrador
         //El administrador confirma que desea crear y se realiza el proceso junto a la base de datos
         private void Crear_confirmacion_Click(object sender, RoutedEventArgs e)
         {
-            if (txtnomusuario.Text != "" && pwboxpassword.Password.ToString() != "" && txtCorreo.Text != ""
-                && txtEstado.Text != "")
+            if (txtnom.Text != "" && txt_rut.Text != "" && txtCorreo.Text != ""
+                && txtEstado.Text != "" && txt_apellido.Text != "")
             {
                 //llamamos a la funcion antes mencionada para obtener que tipo de insumo es y le asignamos un valor local
                 int tipo = tipo_usuario();
                 //Abrimos la conexion con la bd
+
+                 correo      = txtCorreo.Text;
+                 estado      = txtEstado.Text; 
+                 rut         = txt_rut.Text;
+                 nombre      = txtnom.Text;
+                 apellido    = txt_apellido.Text;
+
                 cone.ConnectionString = Conexion;
-                cone.Open();
+               
                 //Realizamos el insert con todos sus parametros necesarios
-                String lector = "insert into usuario (nom_usuario, password ,correo, estado, id_tipo_usuario) values ('" + txtnomusuario.Text + "','" + pwboxpassword.Password.ToString() + "','" + txtCorreo.Text + "', '" + txtEstado.Text + "'," + tipo + ")";
-                OracleDataAdapter adaptador = new OracleDataAdapter(lector, Conexion);
-                DataTable dt = new DataTable();
-                adaptador.Fill(dt);
-                dgUsuarios.ItemsSource = dt.AsDataView();
-                dgUsuarios.Items.Refresh();
+                OracleCommand cmd = new OracleCommand("SP_CREAR_EMPLEADO", cone);
+
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("p_correo", OracleType.Char).Value = correo;
+                cmd.Parameters.Add("p_estado", OracleType.Char).Value = estado;
+                cmd.Parameters.Add("p_rut_empleado", OracleType.Char).Value = rut;
+                cmd.Parameters.Add("p_nombre", OracleType.Char).Value = nombre;
+                cmd.Parameters.Add("p_ap", OracleType.Char).Value = apellido;
+                cmd.Parameters.Add("p_tipo_usuario", OracleType.Number).Value = tipo;
+                cone.Open();
+                cmd.ExecuteNonQuery();
+
+
                 cone.Close();
-            }else
+
+                MessageBox.Show("creacion exitosa");
+                listar();
+            }
+            else
             {
                 MessageBox.Show("faltan campos por rellenar");
             }
